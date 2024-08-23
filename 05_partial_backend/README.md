ee9723d8ec0dc2f15f84a702b98e9ff36ec4eaff
lafayettegabe <gabriel-soares@uergs.edu.br>
2024-08-23 00:13:55 -0300

    add: partial backend

---
+++ 05_partial_backend/README.md
@@ -0,0 +1,13 @@
+## Run
+
+### Development
+
+```
+terraform init -backend-config=dev.tfbackend
+```
+
+### Production
+
+```
+terraform init -backend-config=prod.tfbackend
+```
---
+++ 05_partial_backend/s3.tf
@@ -0,0 +1,7 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 6
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "example-bucket-${random_id.bucket_suffix.hex}"
+}
