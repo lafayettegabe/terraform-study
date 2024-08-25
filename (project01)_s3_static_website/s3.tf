b1474ce24702fd739bdb87d081b128fcbd3cbf5c
lafayettegabe <gabriel-soares@uergs.edu.br>
2024-08-25 19:06:48 -0300

    chore(project01): s3 bucket policy

--- (project01)_s3_static_website/s3.tf
+++ (project01)_s3_static_website/s3.tf
@@ -5,3 +5,29 @@ resource "random_id" "bucket_suffix" {
 resource "aws_s3_bucket" "static_website" {
   bucket = "project01-${random_id.bucket_suffix.hex}"
 }
+
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_website_public_read" {
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+  })
+}
