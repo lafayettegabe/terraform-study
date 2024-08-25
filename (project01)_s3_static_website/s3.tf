7e4d2db6b81eafa15e8c127843622fd73635c04c
lafayettegabe <gabriel-soares@uergs.edu.br>
2024-08-25 19:26:49 -0300

    chore(project01): website configuration and files

--- (project01)_s3_static_website/s3.tf
+++ (project01)_s3_static_website/s3.tf
@@ -31,3 +31,31 @@ resource "aws_s3_bucket_policy" "static_website_public_read" {
     ]
   })
 }
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "index.html"
+  source       = "build/index.html"
+  etag         = filemd5("build/index.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "error.html"
+  source       = "build/error.html"
+  etag         = filemd5("build/error.html")
+  content_type = "text/html"
+}
