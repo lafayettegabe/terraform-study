dce401c48be60bdaa46e5b433efc527ac8e3a7e2
lafayettegabe <gabriel.antunessoares@gmail.com>
2024-09-26 00:47:22 -0300

    add: public modules with ec2

--- 09_public_modules/data.tf
+++ 09_public_modules/data.tf
@@ -1,8 +1,8 @@
 locals {
   project = "09_public_modules"
   region  = "us-east-1"
-}
-
-data "aws_availability_zones" "azs" {
-  state = "available"
+  common_tags = {
+    Project   = local.project
+    Terraform = "true"
+  }
 }
--- 09_public_modules/networking.tf
+++ 09_public_modules/networking.tf
@@ -1,12 +1,20 @@
+locals {
+  vpc_cidr             = "10.0.0.0/16"
+  private_subnet_cidrs = ["10.0.0.0/24"]
+  public_subnet_cidrs  = ["10.0.128.0/24"]
+}
+
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.13"
+  version = "5.5.3"
 
-  name = "09_public_modules"
-
-  azs = data.aws_availability_zones.azs.names
-
-  cidr            = "10.0.0.0/16"
-  private_subnets = ["10.0.0.0/24"]
-  public_subnets  = ["10.0.128.0/24"]
+  name            = local.project
+  azs             = data.aws_availability_zones.azs.names
+  cidr            = local.vpc_cidr
+  private_subnets = local.private_subnet_cidrs
+  public_subnets  = local.public_subnet_cidrs
 }
